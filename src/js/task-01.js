45b6dd50e27f9a4258db1b9aa0d94c70f9b4e527
Aleksandr Moskalyk <alex.moskalik@gmail.com>
2021-08-20 19:07:00 +0300

    фыв

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,14 +1,30 @@
-const allCategories = document.querySelector('#categories');
 
-const allItems = allCategories.querySelectorAll('.item');
 
-console.log(`В списке ${allItems.length} категории.`);
 
-allItems.forEach((li) => {
-    const ItemH2 = li.querySelector('h2');
-    const ItemLi = li.querySelectorAll('li');
+// const allCategories = document.querySelector('#categories');
+// const allItems = allCategories.querySelectorAll('.item');
+
+// console.log(`В списке ${allItems.length} категории.`);
+
+// allItems.forEach((li) => {
+//     const ItemH2 = li.querySelector('h2');
+//     const ItemLi = li.querySelectorAll('li');
       
     
-    console.log(`Категория: ${ItemH2.textContent}`);
-    console.log(`Количество : ${ItemLi.length}`);
-});
+//     console.log(`Категория: ${ItemH2.textContent}`);
+//     console.log(`Количество : ${ItemLi.length}`);
+// });
+
+const list = document.querySelector('#categories')
+console.log(list);
+const firstLi = list.querySelectorAll('.item')
+console.log(firstLi);
+
+
+//работает
+// const secondUl = list.querySelectorAll('ul')
+// console.log(secondUl);
+
+//не работает 
+const secondUl = firstLi.querySelectorAll('ul')
+console.log(secondUl);
